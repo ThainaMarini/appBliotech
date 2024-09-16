0df1ed202e4408babbee0df71bf44298a139e027
João Pedro Thim <joaopedro10thim@gmail.com>
2024-09-16 15:44:28 -0300

    Telas Cadastro Livro, Exibir Emprestimo e Buscar Livro

--- app/src/main/java/com/example/bliotech/CadastroLivroActivity.kt
+++ app/src/main/java/com/example/bliotech/CadastroLivroActivity.kt
@@ -4,14 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bliotech.ui.theme.BliotechTheme
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.TextFieldDefaults
+import kotlin.math.exp
 
 class CadastroLivroActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +51,203 @@ class CadastroLivroActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BliotechTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CadastroLivro(
+                )
+
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CadastroLivro() {
+    var titulo by remember { mutableStateOf("") }
+    var isbn by remember { mutableStateOf("") }
+    var autor by remember { mutableStateOf("") }
+    var generoLiv by remember { mutableStateOf("") }
+    var editora by remember { mutableStateOf("") }
+    var ano by remember { mutableStateOf("") }
+    var msgConfirm by remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
+    val generos = listOf("Ficção", "Horror", "Romance", "Fantasia")
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.2f)
+                .align(Alignment.TopCenter),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Logo
+            val image = painterResource(id = R.drawable.logobliotech)
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            // Titulo
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.95f)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(Color.Black)
+                    .padding(vertical = 8.dp, horizontal = 15.dp), // Padding interno
+            ) {
+                Text(
+                    text = "Cadastro de Livro",
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier.align(Alignment.Center),
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily.Monospace
+                )
+            }
+        }
+
+        // Campos
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .fillMaxHeight()
+                .padding(top = 200.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Título
+            TextField(
+                value = titulo,
+                onValueChange = { titulo = it },
+                label = { Text("Título") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+
+            // ISBN
+            TextField(
+                value = isbn,
+                onValueChange = { isbn = it },
+                label = { Text("ISBN") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+
+            // Autor
+            TextField(
+                value = autor,
+                onValueChange = {},
+                label = { Text("Autor") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+
+            // Editora
+            TextField(
+                value = editora,
+                onValueChange = { editora = it },
+                label = { Text("Editora") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+
+            // Gênero
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = { expanded = !expanded }
+
+            ) {
+                TextField(
+                    value = generoLiv,
+                    onValueChange = { generoLiv = it },
+                    label = { Text("Gênero") },
+                    readOnly = true, // Impede edição direta
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                    },
+                    modifier = Modifier
+                        .menuAnchor()
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    generos.forEach { genero ->
+                        DropdownMenuItem(
+                            text = { Text(genero) },
+                            onClick = {
+                                generoLiv = genero
+                                expanded = false // Fecha o menu ao selecionar
+                            }
+                        )
+                    }
+                }
+            }
+
+            // Ano
+            TextField(
+                value = ano,
+                onValueChange = { ano = it },
+                label = { Text("Ano") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            // Botão de finalizar
+            Button(
+                onClick = { msgConfirm = true },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Green
+                )
+            ) {
+                Text(
+                    text = "Finalizar Cadastro",
+                    fontSize = 18.sp,
+                    color = Color.White
+                )
+            }
+
+            if (msgConfirm){
+                AlertDialog(
+                    onDismissRequest = { msgConfirm = false },
+                    title = { Text(text = "Sucesso")},
+                    text = {Text("Cadastro realizado!")},
+                    confirmButton = {
+                        Button(
+                            onClick = {msgConfirm = false}
+                        ){
+                            Text("Ok")
+                        }
+                    })
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CadastroLivroPreview() {
     BliotechTheme {
-        Greeting("Android")
+        CadastroLivro()
     }
 }
--- app/src/main/java/com/example/bliotech/ExibeEmprestimosActivity.kt
+++ app/src/main/java/com/example/bliotech/ExibeEmprestimosActivity.kt
@@ -4,44 +4,118 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bliotech.ui.theme.BliotechTheme
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.TextFieldDefaults
+import kotlin.math.exp
 
-class ExibeEmprestimosActivity : ComponentActivity() {
+class ExibirEmprestimosActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BliotechTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting4(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ExibirEmprestimos(
+                )
+
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting4(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "aaaaaaaaaaa",
-        modifier = modifier
-    )
+fun ExibirEmprestimos() {
+    var titulo by remember { mutableStateOf("") }
+    var autor by remember { mutableStateOf("") }
+    var generoLiv by remember { mutableStateOf("") }
+    var msgConfirm by remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
+    val generos = listOf("Ficção", "Horror", "Romance", "Fantasia")
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.2f)
+                .align(Alignment.TopCenter),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Logo
+            val image = painterResource(id = R.drawable.logobliotech)
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            // Titulo
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.95f)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(Color.Black)
+                    .padding(vertical = 8.dp, horizontal = 15.dp), // Padding interno
+            ) {
+                Text(
+                    text = "Empréstimoms",
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier.align(Alignment.Center),
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily.Monospace
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview4() {
+fun ExibirEmprestimosPreview() {
     BliotechTheme {
-        Greeting4("Android")
+        ExibirEmprestimos()
     }
 }
