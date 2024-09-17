ce85b21a3c497d3473382b9be83ebe7a812bcfa4
João Pedro Thim <joaopedro10thim@gmail.com>
2024-09-17 18:49:02 -0300

    Tela de Exibir Empréstimo

--- app/src/main/java/com/example/bliotech/ExibeEmprestimosActivity.kt
+++ app/src/main/java/com/example/bliotech/ExibeEmprestimosActivity.kt
@@ -13,6 +13,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,7 +29,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bliotech.ui.theme.BliotechTheme
-import androidx.compose.material3.ExperimentalMaterial3Api
 
 class ExibeEmprestimosActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,7 +78,7 @@ fun ExibeEmprestimos() {
                     .padding(vertical = 8.dp, horizontal = 15.dp), // Padding interno
             ) {
                 Text(
-                    text = "Empréstimoms",
+                    text = "Empréstimos",
                     fontWeight = FontWeight.Bold,
                     color = Color.White,
                     modifier = Modifier.align(Alignment.Center),
@@ -85,6 +87,59 @@ fun ExibeEmprestimos() {
                 )
             }
         }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .fillMaxHeight()
+                .padding(top = 200.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Empréstimos Ativos:",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                modifier = Modifier.padding(bottom = 15.dp),
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Monospace
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.95f)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(Color.LightGray)
+                    .padding(vertical = 8.dp, horizontal = 15.dp), // Padding interno
+            ) {
+                Column(
+                ) {
+                    Text(
+                        text = "Livro: Harry Potter\n" +
+                                "CPF: 123.456.789-10\n" +
+                                "Data Empréstimo: 11/09/24\n" +
+                                "Data Devolução: 18/09/24\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        modifier = Modifier.padding(vertical = 4.dp),
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily.Monospace
+                    )
+                    HorizontalDivider(thickness = 2.dp, color = MaterialTheme.colorScheme.secondary)
+                    Text(
+                        text = "Livro: Cachorrinho Samba\n" +
+                                "CPF: 987.654.321-00\n" +
+                                "Data Empréstimo: 12/09/24\n" +
+                                "Data Devolução: 19/09/24\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily.Monospace
+                    )
+                    HorizontalDivider(thickness = 2.dp, color = MaterialTheme.colorScheme.secondary)
+                }
+            }
+        }
+
     }
 }
 
